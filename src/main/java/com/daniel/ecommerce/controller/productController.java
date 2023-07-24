4bbcb6d7153106126f46ff7d90d770ae50cb97ed
JDanielPerez <juancho.nac13@gmail.com>
2023-07-24 10:32:27 -0500

    24-07-2023

--- src/main/java/com/daniel/ecommerce/controller/productController.java
+++ src/main/java/com/daniel/ecommerce/controller/productController.java
@@ -1,12 +1,17 @@
 package com.daniel.ecommerce.controller;
 
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.daniel.ecommerce.model.product;
 
 @Controller
 @RequestMapping("/products")
 public class productController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(productController.class);
 
     @GetMapping("")
     public String show(){
@@ -17,6 +22,13 @@ public class productController {
     public String create(){
         return "products/create";
     }
+
+    @PostMapping("/save")
+    public String save(product product){
+        LOGGER.info("Este es el objeto producto de la vista {}", product);
+        return "redirect:/product";
+    }
+
 
     
 }
