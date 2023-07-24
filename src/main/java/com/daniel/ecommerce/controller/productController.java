993b7e5d75e93d0c9c2ebbfe1deed403f180b778
JDanielPerez <juancho.nac13@gmail.com>
2023-07-24 11:26:09 -0500

    cambios video 10

--- src/main/java/com/daniel/ecommerce/controller/productController.java
+++ src/main/java/com/daniel/ecommerce/controller/productController.java
@@ -1,18 +1,25 @@
 package com.daniel.ecommerce.controller;
 
 import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.daniel.ecommerce.model.product;
+import com.daniel.ecommerce.model.user;
+import com.daniel.ecommerce.service.productService;
 
 @Controller
 @RequestMapping("/products")
 public class productController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(productController.class);
-
+//  PERMITE OBTENER TODOS LOS METODOS DE LA CLASE
+    @Autowired
+    private productService productService;
+    
     @GetMapping("")
     public String show(){
         return "products/show";
@@ -26,7 +33,10 @@ public class productController {
     @PostMapping("/save")
     public String save(product product){
         LOGGER.info("Este es el objeto producto de la vista {}", product);
-        return "redirect:/product";
+        user u = new user(1,"","","","","","","");
+        product.setUser(u);
+        productService.save(product);
+        return "redirect:/products";
     }
 
 
